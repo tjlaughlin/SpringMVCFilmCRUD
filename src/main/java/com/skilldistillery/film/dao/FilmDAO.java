90b48b9ae10a6baf94f68c3abae39ef597ea7613
Gray Morrow <tgrahammor@gmail.com>
2020-10-17 13:11:23 -0600

    added remaining necessary methods to FilmDAO interface

--- src/main/java/com/skilldistillery/film/dao/FilmDAO.java
+++ src/main/java/com/skilldistillery/film/dao/FilmDAO.java
@@ -1,9 +1,15 @@
 package com.skilldistillery.film.dao;
 
+import java.util.List;
+
 import com.skilldistillery.film.entities.Film;
 
 public interface FilmDAO {
 	
 	public Film findFilmById(int filmId);
+	public Film addFilm( Film film);
+	public boolean deleteFilm(int filmId);
+	public boolean editFilm( int filmId );
+	public List<Film> findFilmsByKeyword( String keyword );
 	
 }
--- src/main/java/com/skilldistillery/film/dao/JDBCFilmDAOImpl.java
+++ src/main/java/com/skilldistillery/film/dao/JDBCFilmDAOImpl.java
@@ -1,5 +1,20 @@
 package com.skilldistillery.film.dao;
 
-public class JDBCFilmDAOImpl {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.skilldistillery.film.entities.Film;
+
+public class JDBCFilmDAOImpl implements FilmDAO {
+	
+	static {
+		try {
+			Class.forName( "com.mysql.jdbc.Driver" ) ;
+		} catch ( ClassNotFoundException e ) {
+			System.out.println( "ERROR! CLASS DEFINITION NOT FOUND!" ) ;
+		}
+	}
 
 }
+
