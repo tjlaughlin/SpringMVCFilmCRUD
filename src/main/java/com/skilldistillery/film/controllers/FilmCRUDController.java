af5c0ef5968ff11d5293a937ac66604b79a7128b
Daniel Majewski <d.majewski.3453@gmail.com>
2020-10-18 00:20:57 -0600

    controller

--- src/main/java/com/skilldistillery/film/controllers/FilmCRUDController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmCRUDController.java
@@ -35,108 +35,108 @@ public class FilmCRUDController {
 
 	@RequestMapping(path = "FilmsList.do", method = RequestMethod.GET)
 	public ModelAndView getByKeyword(String keyword) {
-		ModelAndView mv = new ModelAndView(); 
-		
+		ModelAndView mv = new ModelAndView();
+
 		mv.setViewName("resultList");
 		mv.addObject("filmList", filmDAO.findFilmsByKeyword(keyword));
 		return mv;
 	}
-	
-	@RequestMapping( path = "deleteFilm.do" )
-	public ModelAndView deleteFilm( int filmId ) {
-		
-		ModelAndView mv  = new ModelAndView();
+
+	@RequestMapping(path = "deleteFilm.do")
+	public ModelAndView deleteFilm(int filmId) {
+
+		ModelAndView mv = new ModelAndView();
 		mv.setViewName("delete");
 		mv.addObject("successful", filmDAO.deleteFilm(filmId));
-		
+
 		return mv;
-		
+
 	}
-	
-	
 
-	@RequestMapping(path="createFilm.do",
-            method=RequestMethod.POST)
+	@RequestMapping(path = "createFilm.do", method = RequestMethod.POST)
 	public ModelAndView newFilm(@ModelAttribute("film") Film film, RedirectAttributes redir) {
-	    film = filmDAO.addFilm(film);
-	    ModelAndView mv = new ModelAndView();
-	    redir.addFlashAttribute("film", film);
-	    mv.setViewName("redirect:filmCreated.do");
-	    return mv;
-	  }
+		film = filmDAO.addFilm(film);
+		ModelAndView mv = new ModelAndView();
+		redir.addFlashAttribute("film", film);
+		mv.setViewName("redirect:filmCreated.do");
+		return mv;
+	}
 
-	  @RequestMapping(path="filmCreated.do",
-	                  method=RequestMethod.GET)
-	  public ModelAndView createdFilm() {
-	    ModelAndView mv = new ModelAndView();
-	    mv.addObject("film");
-	    mv.setViewName("added");
-	    return mv;
-	  }
-	
-	
-	
-	@RequestMapping( path = "FilmsUpdate.do", method = RequestMethod.POST, params = "title")
-	public ModelAndView editFilmTitle(int id, String title ) {
-		ModelAndView mv  = new ModelAndView();
+	@RequestMapping(path = "filmCreated.do", method = RequestMethod.GET)
+	public ModelAndView createdFilm() {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("film");
+		mv.setViewName("added");
+		return mv;
+	}
+
+	@RequestMapping(path = "FilmsUpdate.do", method = RequestMethod.POST, params = "title")
+	public ModelAndView editFilmTitle(int id, String title) {
+		ModelAndView mv = new ModelAndView();
 		mv.setViewName("updated");
 		mv.addObject("updatedFilmId", Integer.valueOf(id));
 
 		mv.addObject("film", filmDAO.editFilm("title", title, id));
 		return mv;
 	}
-	@RequestMapping( path = "FilmsUpdate.do", method = RequestMethod.POST, params = "description")
-	public ModelAndView editFilmDescription(int id, String description ) {
-		ModelAndView mv  = new ModelAndView();
+
+	@RequestMapping(path = "FilmsUpdate.do", method = RequestMethod.POST, params = "description")
+	public ModelAndView editFilmDescription(int id, String description) {
+		ModelAndView mv = new ModelAndView();
 		mv.setViewName("updated");
 		mv.addObject("updatedFilmId", Integer.valueOf(id));
 
 		mv.addObject("film", filmDAO.editFilm("description", description, id));
 		return mv;
 	}
-	@RequestMapping( path = "FilmsUpdate.do", method = RequestMethod.POST, params = "releaseYear")
-	public ModelAndView editReleaseYear(int id, String releaseYear ) {
-		ModelAndView mv  = new ModelAndView();
+
+	@RequestMapping(path = "FilmsUpdate.do", method = RequestMethod.POST, params = "releaseYear")
+	public ModelAndView editReleaseYear(int id, String releaseYear) {
+		ModelAndView mv = new ModelAndView();
 		mv.setViewName("updated");
 		mv.addObject("updatedFilmId", Integer.valueOf(id));
 
 		mv.addObject("film", filmDAO.editFilm("release_year", releaseYear, id));
 		return mv;
 	}
-	@RequestMapping( path = "FilmsUpdate.do", method = RequestMethod.POST, params = "rentalDuration")
-	public ModelAndView editRentalDuration(int id, String rentalDuration ) {
-		ModelAndView mv  = new ModelAndView();
+
+	@RequestMapping(path = "FilmsUpdate.do", method = RequestMethod.POST, params = "rentalDuration")
+	public ModelAndView editRentalDuration(int id, String rentalDuration) {
+		ModelAndView mv = new ModelAndView();
 		mv.setViewName("updated");
 		mv.addObject("updatedFilmId", Integer.valueOf(id));
 
 		mv.addObject("film", filmDAO.editFilm("rental_duration", rentalDuration, id));
 		return mv;
 	}
-	@RequestMapping( path = "FilmsUpdate.do", method = RequestMethod.POST, params = "rentalRate")
-	public ModelAndView editRentalRate(int id, String rentalRate ) {
-		ModelAndView mv  = new ModelAndView();
+
+	@RequestMapping(path = "FilmsUpdate.do", method = RequestMethod.POST, params = "rentalRate")
+	public ModelAndView editRentalRate(int id, String rentalRate) {
+		ModelAndView mv = new ModelAndView();
 		mv.setViewName("updated");
 		mv.addObject("updatedFilmId", Integer.valueOf(id));
 
 		mv.addObject("film", filmDAO.editFilm("rental_rate", rentalRate, id));
 		return mv;
 	}
-	@RequestMapping( path = "FilmsUpdate.do", method = RequestMethod.POST, params = "replacementCost")
-	public ModelAndView editReplacementCost(int id, String replacementCost ) {
-		ModelAndView mv  = new ModelAndView();
+
+	@RequestMapping(path = "FilmsUpdate.do", method = RequestMethod.POST, params = "replacementCost")
+	public ModelAndView editReplacementCost(int id, String replacementCost) {
+		ModelAndView mv = new ModelAndView();
 		mv.setViewName("updated");
 		mv.addObject("updatedFilmId", Integer.valueOf(id));
 
 		mv.addObject("film", filmDAO.editFilm("replacement_cost", replacementCost, id));
 		return mv;
 	}
-	@RequestMapping( path = "FilmsUpdate.do", method = RequestMethod.POST, params = "length")
-	public ModelAndView editLength(int id, String length ) {
-		ModelAndView mv  = new ModelAndView();
+
+	@RequestMapping(path = "FilmsUpdate.do", method = RequestMethod.POST, params = "length")
+	public ModelAndView editLength(int id, String length) {
+		ModelAndView mv = new ModelAndView();
 		mv.setViewName("updated");
 		mv.addObject("updatedFilmId", Integer.valueOf(id));
 		mv.addObject("film", filmDAO.editFilm("replacement_cost", length, id));
 		return mv;
 	}
-	
+
 }
